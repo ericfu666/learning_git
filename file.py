3761891aa4324fbfa18b4145af289f01d9a37a59
ericfu <ericfu666@outlook.com>
2021-06-06 00:24:30 +0800

    resolved

--- file.py
+++ file.py
@@ -2,10 +2,16 @@
 def product(x,y):
     prod=x*y
     return prod
+
 def sum(x,y):
     s=x+y
     return s
 
 
+def sub(x,y):
+    return x-y
+
+
 x=100000;y=200000
 print(product(x,y))
+print(sub(x,y))
